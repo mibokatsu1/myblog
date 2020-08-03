6ae37d81a7209dcc38b8b5446a15c6bed4141002
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-03 23:26:12 +0900

    add routes index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
