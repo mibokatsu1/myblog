4ced356f35b335f03b534b62120e232ac95c4538
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-05 16:38:39 +0900

    add css into topPage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -48,6 +48,11 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+
+        </ul>
+      </div>
     </nav>
 
   
