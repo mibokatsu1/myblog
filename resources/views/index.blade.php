860d2d1194f265ce6551c77ed85d2a3ef0b1fb78
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-05 17:53:16 +0900

    ajust css into topPage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,50 +42,52 @@
 
   <div id="app">
     <nav class="navbar">
-      <a class="navbar-brand" href="{{ url('/') }}">
-        {{ config('app.name', 'Laravel') }}
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav mr-auto">
+      <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
 
-        </ul>
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-          <!-- Authentication Links -->
-          ゲスト
-        
-          <li class="nav-item">    
-          <li class="nav-item">
-              <a class="nav-link" href="{{ url('/') }}">User</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="{{ url('/') }}">Post</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="{{ url('/') }}">Login</a>
-          </li>
-          register
-          <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              ユーザー名
-            </a>
-
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ url('/') }}">
-                ログアウト
+          </ul>
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            ゲスト
+          
+            <li class="nav-item">    
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/') }}">User</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/') }}">Post</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/') }}">Login</a>
+            </li>
+            register
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                ユーザー名
               </a>
 
-              <form id="logout-form" action="{{ url('#') }}" method="POST">
-                フォーム
-              </form>
-            </div>
-          </li>
-        </ul>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ url('/') }}">
+                  ログアウト
+                </a>
 
+                <form id="logout-form" action="{{ url('#') }}" method="POST">
+                  フォーム
+                </form>
+              </div>
+            </li>
+          </ul>
+
+        </div>
       </div>
     </nav>
 
