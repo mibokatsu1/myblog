be2383116145a67fe4541892817ee87b2ff83be4
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-06 23:02:03 +0900

    extend table & guarded

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // 初期設定
+    protected $table = 'posts';
+    // ブラックリスト_複数代入時に代入を許可しない属性を配列で設定
+    protected $guarded = array('id');
 }
