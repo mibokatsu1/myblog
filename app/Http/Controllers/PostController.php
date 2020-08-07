5e6c605508d6446c2b41feb2011371a4af64fac2
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-07 15:04:15 +0900

    add Valid into store-action

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,6 +43,13 @@ class PostController extends Controller
     {
         $post = new Post;
         $form = $request->all();
+
+        // バリデーション
+        $rules = [
+            'user_id' => 'integer|required', // 2項目以上条件がある場合は「 | 」を挟む
+            'title' => 'required',
+            'message' => 'required',
+        ];
     }
 
     /**
