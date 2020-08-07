46b2c24493993c00feee0371c3159dad1854fa6c
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-07 16:52:50 +0900

    export Post-table title into topPage

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -112,6 +112,22 @@
       <input type="submit" class="create" value="投  稿">
     </form>
 
+    <!-- 記事描画部分 -->
+    @if(count($items) > 0)
+    @foreach($items as $item)
+        <div class="alert alert-primary" role="alert">
+            <a href="/post/{{ $item->id }}" class="alert-link">{{ $item->title }}</a>
+            <form action="/post/{{ $item->id }}" method="POST">
+            @csrf
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="submit" class="delete" value="削除">
+            </form>
+        </div>
+    @endforeach
+    @else
+        <div>投稿記事がありません</div>
+    @endif
+
     <div class="test">
       Hellow World
     </div>
