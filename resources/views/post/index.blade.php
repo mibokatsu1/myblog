a3dbc445426d2bb5ef15fb31c1c5b9b49874122e
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-07 16:36:10 +0900

    add error message into from(post)

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -96,7 +96,16 @@
     <!-- 投稿フォーム -->
     <form action="post" method="post">
       @csrf
+
+      <input type="hidden" name="user_id" value="1">
+      @if($errors->has('title'))
+        <div class="error_msg">{{ $errors->first('title') }}</div>
+      @endif
       <input type="text" class="form" name="title" placeholder="タイトル" value="{{ old('title') }}">
+
+      @if($errors->has('message'))
+        <div class="error_msg">{{ $errors->first('message') }}</div>
+      @endif
       <div>
           <textarea class="form" name="message" placeholder="メッセージ">{{ old('message') }}</textarea>
       </div>
