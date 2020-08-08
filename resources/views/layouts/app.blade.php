93ecc288b5867c636fc7dba036f7a87e69dae6e6
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-08 23:10:04 +0900

    add user-name into topPage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,10 +44,10 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="{{ url('/post') }}">
-          {{ config('app.name', 'Laravel') }}
+            {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse">
           <!-- Left Side Of Navbar -->
@@ -84,15 +84,14 @@
                     <a class="nav-link" href="{{ url('/') }}">Login</a>
                 </li>
 
-
                 <li class="nav-item dropdown">
                   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ url('/') }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    ユーザー名
+                      {{ Auth::user()->name }} <span class="caret"></span>
                   </a>
 
                   <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="{{ url('/') }}">
-                      ログアウト
+                        ログアウト
                     </a>
 
                     <form id="logout-form" action="{{ url('#') }}" method="POST">
