81d701af302608e67e0b0600de8d54f70a2e1fc6
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-08 11:54:45 +0900

    move footer-directory into app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,6 +92,22 @@
     <main class="py-4">
         @yield('content')
     </main>
+
+    <!-- フッター -->
+    <div class="test">
+        フッター
+    </div>
+
+    <div class="links">
+      <a href="https://laravel.com/docs"><button class='btn btn-default'>Docs</button></a>
+      <a href="https://laracasts.com"><button class='btn btn-primary'>Laracasts</button></a>
+      <a href="https://laravel-news.com"><button class='btn btn-success'>News</button></a>
+      <a href="https://blog.laravel.com"><button class='btn btn-info'>Blog</button></a>
+      <a href="https://nova.laravel.com"><button class='btn btn-warning'>Nova</button></a>
+      <a href="https://forge.laravel.com"><button class='btn btn-danger'>Forge</button></a>
+      <a href="https://vapor.laravel.com"><button class='btn btn-link'>Vapor</button></a>
+      <a href="https://github.com/laravel/laravel"><button class='btn btn-primary'>GitHub</button></a>
+    </div>
   </div>
 
   <script src="{{ mix('js/app.js') }}" defer></script>
