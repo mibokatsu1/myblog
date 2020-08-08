fc6afd7dd0fb599ae869cad35a13d465b9b2806c
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-08 22:05:28 +0900

    add authUser into topPage

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -7,7 +7,9 @@
     <form action="post" method="post">
         @csrf
 
-        <input type="hidden" name="user_id" value="1">
+        <!-- 認証ユーザーのid取得 -->
+        <input type="hidden" name="user_id" value="{{ $authUser->id }}">
+        <!-- <input type="hidden" name="user_id" value="1">  ユーザー登録機能実装前、仮置き -->
         @if($errors->has('title'))
             <div class="error_msg">{{ $errors->first('title') }}</div>
         @endif
@@ -24,17 +26,21 @@
 
     <!-- 記事描画部分 -->
     @if(count($items) > 0)
-    @foreach($items as $item)
-        <div class="alert alert-primary" role="alert">
-            <a href="/post/{{ $item->id }}" class="alert-link">{{ $item->title }}</a>
-            <div class="created_at">{{ $item->created_at}}</div>
-            <form action="/post/{{ $item->id }}" method="POST">
-            @csrf
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="submit" class="delete" value="削除">
-            </form>
-        </div>
-    @endforeach
+        @foreach($items as $item)
+            <div class="alert alert-primary" role="alert">
+                <a href="/post/{{ $item->id }}" class="alert-link">{{ $item->title }}</a>
+                <div class="created_at">{{ $item->created_at}}</div>
+
+                <!-- 主キーと外部キーが同じ場合 -> リンク付きテキストと削除ボタン表示 -->
+                @if($authUser->id === $item->user_id)
+                <form action="/post/{{ $item->id }}" method="POST">
+                @csrf
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="submit" class="delete" value="削除">
+                </form>
+                @endif
+            </div>
+        @endforeach
 
     @else
         <div>投稿記事がありません</div>
