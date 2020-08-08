dc63e691bacf02a30a153689b1c4b04044de9573
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-08 17:53:31 +0900

    add update into show-view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -8,6 +8,18 @@
 
         <div class="headcopy">Message</div><hr>
         <div class="text">{{ $item->message }}</div>
+
+        <form action="/post/{{$item->id}}" method="POST">
+            @csrf
+
+            <input type="hidden" name="user_id" value="1">
+            <input type="text" class="form" name="title" placeholder="タイトル" value="{{ $item->title }}">
+            <div>
+                <textarea class="form" name="message" placeholder="メッセージ">{{ $item->message }}</textarea>
+            </div>
+            <input type="hidden" name="_method" value="PUT">
+            <input type="submit" class="update" value="変　更">
+        </form>
     @endif
 
     <a href="/post" class="edit">編集する</a>
