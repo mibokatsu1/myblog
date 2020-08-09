274b90778983d5cd76b85eee309887dfdca3a087
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-09 10:47:58 +0900

    chage webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.browserSync('myblog.test')
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .version();
+  .sass('resources/assets/sass/app.scss', 'public/css')
+  // .sass('resources/assets/sass/_base.scss', '../resources/assets/build/css')
+   // importファイルは逐一追加する(しないとwatch-pollで監視できない)
+  .sass('resources/assets/sass/style.scss', 'public/css/style.css') // assets/sass配下のstyle.scssを、public/css配下にstyle.cssとしてコンパイル
+  .js('resources/assets/js/app.js', 'public/js')
+  
+  // .js('resources/assets/sass/style.scss', 'public/js')
+  .version();
