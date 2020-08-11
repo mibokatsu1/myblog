192223b682725bc804af23f0e8e65ea71929f09e
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-11 18:01:17 +0900

    create images-table

--- database/migrations/2020_08_11_175404_create_images_table.php
+++ database/migrations/2020_08_11_175404_create_images_table.php
@@ -15,6 +15,8 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id');
+            $table->string('file_name');
             $table->timestamps();
         });
     }
