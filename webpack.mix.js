2196439db76b5946e319dde46d8f2fed7c2b0f05
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-13 22:20:09 +0900

    remove unnecessary description

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,6 @@ const mix = require('laravel-mix');
 mix.browserSync('myblog.test')
   // .sass('resources/assets/sass/app.scss', 'public/app.css')
 
-  // .sass('resources/assets/sass/_base.scss', '../resources/assets/build/css')
-   // importファイルは逐一追加する(しないとwatch-pollで監視できない)
   // .sass('resources/assets/sass/style.scss', 'public/css/style.css')
    // assets/sass配下のstyle.scssを、public/css配下にstyle.cssとしてコンパイル
 
